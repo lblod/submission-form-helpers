07bbfd9c451387982251697d589b6789733a1f31
felix <ruiz.felixruiz@gmail.com>
2022-03-22 16:23:24 +0100

    Update to also allow to exclude from a list

--- constraints/match-values.js
+++ constraints/match-values.js
@@ -1,13 +1,28 @@
-import { FORM, SKOS } from '../namespaces';
+import { FORM } from '../namespaces';
 
 /**
  * For an arbitrary list of elements, check whether the provided value matches one element of the list
  */
 
 export default function constraintMatchValues(values, options) {
-  const {constraintUri, store, metaGraph} = options;
-  const targetValues = store
-        .match(constraintUri, FORM('valueElement'), undefined)
-        .map(triple => triple.object.value);
-  return values.some(value => targetValues.includes(value.value));
+  const {constraintUri, store } = options;
+  const targetValuesIn = store.match(constraintUri, FORM('valuesIn'), undefined);
+  const targetValuesNotIn = store.match(constraintUri, FORM('valuesNotIn'), undefined);
+
+  if(values.length == 0) {
+    //We don't want to evaluate if there is nothing on this path
+    return false;
+  }
+
+  else if(targetValuesIn.length) {
+    const elements = targetValuesIn[0].object.elements.map(element => element.value);
+    return values.filter(value => elements.includes(value.value)).length > 0;
+  }
+
+  else if(targetValuesNotIn.length) {
+    const elements = targetValuesNotIn[0].object.elements.map(element => element.value);
+    return values.filter(value => elements.includes(value.value)).length == 0;
+  }
+
+  else return false;
 }
