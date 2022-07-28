dd9adbaa3e9416e371c8f5272113ae3a1ad9ae18
Sergey Andreev <sergey.andreev@redpencil.io>
2022-07-28 18:16:05 +0200

    changed the algo for checking

--- constraints/required.js
+++ constraints/required.js
@@ -1,12 +1,16 @@
-export default function constraintsRequired(values/*, options*/) {
+import { FORM } from '../namespaces';
+
+export default function constraintsRequired(values, options) {
   const hasValues = values.length > 0 ? true : false;
   if(!hasValues){
     return false;
   }
-
-  for(const value of values){
-    if(value.value.length == 0){
-      return false;
+  //for input with multiple languages
+  const language = options.store.match( options.constraintUri, FORM('language'), undefined)[0].object.value;
+  if(language){
+    const value=values.find(value=>value.language==language);
+    if(value?.value?.length==0){
+      return false
     }
   }
   return true;
