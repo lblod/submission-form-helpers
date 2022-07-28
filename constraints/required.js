1da7dfdcd3d80f9967a436e8fd5e0c84db243677
Sergey Andreev <sergey.andreev@redpencil.io>
2022-07-28 19:37:07 +0200

    fixed some edge cases

--- constraints/required.js
+++ constraints/required.js
@@ -1,16 +1,22 @@
 import { FORM } from '../namespaces';
 
 export default function constraintsRequired(values, options) {
-  const hasValues = values.length > 0 ? true : false;
-  if(!hasValues){
+  if(values.length === 0){
     return false;
   }
-  //for input with multiple languages
-  const language = options.store.match( options.constraintUri, FORM('language'), undefined)[0]?.object?.value;
-  if(language){
-    const value=values.find(value=>value.language==language);
-    if(value?.value?.length==0){
-      return false
+  else if(values.length === 1){
+    if(values[0].value?.length === 0){
+      return false;
+    }
+  }
+  else if(values.length > 1){
+    //for input with multiple languages
+    const language = options.store.match( options.constraintUri, FORM('language'), undefined)[0]?.object?.value;
+    if(language){
+      const value=values.find(value=>value.language==language);
+      if(value?.value?.length === 0){
+        return false
+      }
     }
   }
   return true;
