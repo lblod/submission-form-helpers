52701c3943df562c33387530a95cbd53b34abf17
Sergey Andreev <sergey.andreev@redpencil.io>
2022-07-28 16:40:32 +0200

    added chec for every value

--- constraints/required.js
+++ constraints/required.js
@@ -1,3 +1,13 @@
 export default function constraintsRequired(values/*, options*/) {
-  return (values.length > 0 && true) || false;
+  const hasValues = values.length > 0 ? true : false;
+  if(!hasValues){
+    return false;
+  }
+
+  for(const value of values){
+    if(value.value.length == 0){
+      return false;
+    }
+  }
+  return true;
 }
