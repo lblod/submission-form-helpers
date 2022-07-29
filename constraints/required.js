b919b6c239d0214a29b87c1ccd3a8f0597ca4738
Sergey Andreev <sergey.andreev@redpencil.io>
2022-07-29 15:55:43 +0200

    changed logic requiring that at least one of them is not empty

--- constraints/required.js
+++ constraints/required.js
@@ -2,34 +2,40 @@ import { FORM } from '../namespaces';
 
 export default function constraintsRequired(values, options) {
   //no values
-  if(values.length === 0){
+  if (values.length === 0) {
     return false;
   }
   //1 or more values/input boxes
-  else if(values.length > 0){
+  else if (values.length > 0) {
     //check if there is a language constraint
-    const languageConstraint = options.store.match( options.constraintUri, FORM('language'), undefined)[0]?.object?.value;
-    if(languageConstraint){
+    const languageConstraint = options.store.match(options.constraintUri, FORM('language'), undefined)[0]?.object?.value;
+    if (languageConstraint) {
       //match the value to the constraint
       //this is wierd since it will fail if there are multiple same language paths
-      const value = values.find(value=>value.language === languageConstraint);
-      if(!value){
+      const value = values.find(value => value.language === languageConstraint);
+      if (!value) {
         return false;
       }
-      else if(value?.value?.length === 0){
+      else if (value?.value?.length === 0) {
         return false;
       }
 
     }
-    else{
+    else {
       //no way to determine which box is validated so we validate all of them
       //this will cause ui bugs
+      let allEmpty = true;
       for (const value of values) {
-        if(value.value?.length === 0){
-          return false;
+        if (value.value?.length > 0) {
+          allEmpty = false;
+          break;
         }
+      }
+      if (allEmpty) {
+        return false;
       }
     }
   }
+
   return true;
 }
