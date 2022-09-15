0bd079c85de32d6f9159a72eca16dc00c32ffd90
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2022-09-15 11:43:25 +0200

    Set up Prettier
    This sets up prettier as a standalone npm script so we can run it on all
    supported file types.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,7 @@ module.exports = {
     node: true,
     es2021: true,
   },
-  extends: ["eslint:recommended", "plugin:n/recommended"],
+  extends: ["eslint:recommended", "plugin:n/recommended", "prettier"],
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
