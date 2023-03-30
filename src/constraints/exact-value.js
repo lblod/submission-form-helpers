25893676bf9a53830ce5cffaf24372212591af3c
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2023-03-30 11:24:23 +0200

    Add array support to the `exact value` constraint
    Some form fields always return an array but we still want to check if the selected value matches the predefined one. An array is considered valid if it contains only a single literal, and the literal value is the same as the defined `customValue`.

--- src/constraints/exact-value.js
+++ src/constraints/exact-value.js
@@ -7,5 +7,10 @@ import { FORM } from "../namespaces.js";
 export default function constraintExactValue(value, options) {
   const { constraintUri, store } = options;
   const expected = store.any(constraintUri, FORM("customValue"), undefined);
-  return value.value == expected.value;
+
+  if (Array.isArray(value)) {
+    return value.length === 1 && value[0].value === expected.value;
+  } else {
+    return value.value == expected.value;
+  }
 }
