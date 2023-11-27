7f058f0505c9f3f09a9e44368910f80de1d512b8
Karel Kremer <karel.kremer@redpencil.io>
2023-11-27 15:18:54 +0100

    add negative path constraints

--- src/triples-for.js
+++ src/triples-for.js
@@ -173,7 +173,7 @@ function triplesForComplexPath(options, createMissingNodes = false) {
     orderedSegmentData.push(segmentData);
   }
 
-  // (this is reduntant, if there are no startingPoints values will
+  // (this is redundant, if there are no startingPoints values will
   // always be an array, but it's more obvious ;-)
   if (nextPathElements.length == 0)
     return {
@@ -184,34 +184,24 @@ function triplesForComplexPath(options, createMissingNodes = false) {
   else return { triples: datasetTriples, values: [], orderedSegmentData };
 }
 
-//Note: scope can match multiple nodes
-export function triplesForScope(scopeUri, options) {
-  const { store, formGraph, sourceNode, sourceGraph } = options;
-  let path = store.any(scopeUri, SHACL("path"), undefined, formGraph);
-  const dataset = triplesForPath({
-    store,
-    path,
-    formGraph,
-    sourceNode,
-    sourceGraph,
-  });
-
-  //TODO: probably this could be more performant
-  //TODO: support other types of constraints?
-  let constraints = store
-    .match(scopeUri, FORM("constraint"), undefined, formGraph)
-    .filter((constraint) =>
-      store.any(constraint.object, RDF("type"), SHACL("NodeShape"), formGraph)
-    )
-    .map((constraint) =>
-      store.match(constraint.object, SHACL("property"), undefined, formGraph)
-    )
+function toPathConstraints(rawConstraints, scopeOptions, negative = false) {
+  const { store, formGraph } = scopeOptions;
+  return rawConstraints
+    .map((constraint) => {
+      return store.match(
+        constraint.object,
+        SHACL("property"),
+        undefined,
+        formGraph
+      );
+    })
     .reduce((acc, properties) => {
       return [...acc, ...properties];
     }, [])
     .map((property) => {
       return {
         path: store.any(property.object, SHACL("path"), undefined, formGraph),
+        negative,
         targetNode: store.any(
           property.object,
           SHACL("targetNode"),
@@ -220,23 +210,75 @@ export function triplesForScope(scopeUri, options) {
         ),
       };
     });
+}
+
+function getNodeShapeConstraints(constraints, scopeOptions) {
+  const { store, formGraph } = scopeOptions;
+  return toPathConstraints(
+    constraints.filter((constraint) =>
+      store.any(constraint.object, RDF("type"), SHACL("NodeShape"), formGraph)
+    ),
+    scopeOptions,
+    false
+  );
+}
+
+function getNegativeNodeShapeConstraints(constraints, scopeOptions) {
+  const { store, formGraph } = scopeOptions;
+  return toPathConstraints(
+    constraints.filter((constraint) =>
+      store.any(
+        constraint.object,
+        RDF("type"),
+        SHACL("NegativeNodeShape"),
+        formGraph
+      )
+    ),
+    scopeOptions,
+    true
+  );
+}
+
+function constraintMatches(constraint, value, scopeOptions) {
+  const { store, formGraph, sourceGraph } = scopeOptions;
+  const objects = triplesForPath({
+    store,
+    path: constraint.path,
+    formGraph,
+    sourceNode: value,
+    sourceGraph,
+  }).values;
+
+  const matchFound = objects.find((o) => o.equals(constraint.targetNode));
+  return constraint.negative ? !matchFound : matchFound;
+}
+
+function checkConstraintsOnLastPathSegment(
+  constraints,
+  scopeUri,
+  scopeOptions
+) {
+  const { store, formGraph, sourceNode, sourceGraph } = scopeOptions;
+  let path = store.any(scopeUri, SHACL("path"), undefined, formGraph);
+
+  const triplesMatchingPath = triplesForPath({
+    store,
+    path,
+    formGraph,
+    sourceNode,
+    sourceGraph,
+  });
 
   // filter out values which don't match shapes
   // Note: the filtered data changes  are applied in place
-  const orderedSegmentData = dataset.orderedSegmentData;
+  const orderedSegmentData = triplesMatchingPath.orderedSegmentData;
   const lastSegmentData = orderedSegmentData.slice(-1)[0];
 
   for (const value of [...lastSegmentData.values]) {
     for (const constraint of constraints) {
-      const objects = triplesForPath({
-        store,
-        path: constraint.path,
-        formGraph,
-        sourceNode: value,
-        sourceGraph,
-      }).values;
+      const match = constraintMatches(constraint, value, scopeOptions);
 
-      if (!objects.find((o) => o.equals(constraint.targetNode))) {
+      if (!match) {
         lastSegmentData.values = lastSegmentData.values.filter(
           (v) => !v.equals(value)
         );
@@ -253,6 +295,38 @@ export function triplesForScope(scopeUri, options) {
       }
     }
   }
+
+  return { orderedSegmentData, lastSegmentData };
+}
+
+//Note: scope can match multiple nodes
+export function triplesForScope(scopeUri, scopeOptions) {
+  const { store, formGraph } = scopeOptions;
+
+  //TODO: probably this could be more performant
+  const allConstraints = store.match(
+    scopeUri,
+    FORM("constraint"),
+    undefined,
+    formGraph
+  );
+
+  const nodeShapeConstraints = getNodeShapeConstraints(
+    allConstraints,
+    scopeOptions
+  );
+  const negativeNodeShapeConstraints = getNegativeNodeShapeConstraints(
+    allConstraints,
+    scopeOptions
+  );
+  //TODO: support other types of constraints?
+  const constraints = [
+    ...nodeShapeConstraints,
+    ...negativeNodeShapeConstraints,
+  ];
+
+  const { orderedSegmentData, lastSegmentData } =
+    checkConstraintsOnLastPathSegment(constraints, scopeUri, scopeOptions);
 
   const filteredTriples = orderedSegmentData.reduce((acc, segment) => {
     return [...acc, ...segment.triples];
