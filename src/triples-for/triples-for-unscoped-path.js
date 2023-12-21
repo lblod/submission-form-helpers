76633ca632f91378f2720d0179215b077ee36211
felix <ruiz.felixruiz@gmail.com>
2023-12-21 16:26:35 +0100

    WIP

--- src/triples-for/triples-for-unscoped-path.js
+++ src/triples-for/triples-for-unscoped-path.js
@@ -1,6 +1,6 @@
 import { NamedNode, Statement } from "rdflib";
 import { v4 as uuidv4 } from "uuid";
-import { SHACL } from "../namespaces.js";
+import { SHACL, RDF, FORM } from "../namespaces.js";
 import { URI_TEMPLATE } from "../constants.js";
 
 export function triplesForUnscopedPath(
@@ -10,6 +10,10 @@ export function triplesForUnscopedPath(
 ) {
   const { path } = options;
 
+  // TODO: how would we deal with?
+  // path [ sh:inversePath ext:theInversePredicate ];
+  // (and which should be equivalent and which isn't)
+  // ( [ sh:inversePath ext:theInversePredicate ] )
   if (path && path.termType === "Collection") {
     return triplesForComplexPath(options, pathInfo, createMissingNodes);
   } else {
@@ -28,6 +32,9 @@ function triplesForSimplePath(options, pathInfo, createMissingNodes = false) {
     for (const sourceNode of startNodes) {
       const triples = store.match(sourceNode, path, undefined, sourceGraph);
 
+      // This is very obfuscated way of creating a
+      // stub-triple so consumers (i.e. components)
+      // can work with the created triple
       if (createMissingNodes && triples.length == 0) {
         triples.push(
           new Statement(
@@ -103,14 +110,38 @@ function triplesForComplexPath(options, pathInfo, createMissingNodes = false) {
         );
 
         if (createMissingNodes && triples.length == 0) {
+          const newSubject = new NamedNode(URI_TEMPLATE + uuidv4());
+
+          // This is very obfuscated way of creating a
+          // stub-triple so consumers (i.e. components)
+          // can work with the created triple
           triples.push(
             new Statement(
-              new NamedNode(URI_TEMPLATE + uuidv4()),
+              newSubject,
               currentPathElement.inversePath,
               startingPoint,
               sourceGraph
             )
           );
+
+          // If hops are created, we give them here a rdf:type
+          // Most systems really like types.
+          // Why the penultimate pathElement?
+          // Because we only care about intermediate hops
+          // and their types.
+          // The last pathElement will match the triple
+          // the component works on and should
+          // be managed by this.
+          if(nextPathElements.length > 1) {
+            store.addAll([
+             new Statement(
+               newSubject,
+               RDF('type'),
+               FORM('FormDataNode'),
+               sourceGraph
+             )
+            ]);
+          }
         }
 
         triples.map((triple) => {
@@ -128,14 +159,38 @@ function triplesForComplexPath(options, pathInfo, createMissingNodes = false) {
         );
 
         if (createMissingNodes && triples.length == 0) {
+
+          // This is very obfuscated way of creating a
+          // stub-triple so consumers (i.e. components)
+          // can work with the created triple
+          const newSubject = new NamedNode(URI_TEMPLATE + uuidv4());
           triples.push(
             new Statement(
               startingPoint,
               currentPathElement.path,
-              new NamedNode(URI_TEMPLATE + uuidv4()),
+              newSubject,
               sourceGraph
             )
           );
+
+          // If hops are created, we give them here a rdf:type
+          // Most systems really like types.
+          // Why the penultimate pathElement?
+          // Because we only care about intermediate hops
+          // and their types.
+          // The last pathElement will match the triple
+          // the component works on and should
+          // be managed by this.
+          if(nextPathElements.length > 1) {
+            store.addAll([
+             new Statement(
+               newSubject,
+               RDF('type'),
+               FORM('FormDataNode'),
+               sourceGraph
+             )
+            ]);
+          }
         }
 
         triples.map((triple) => {
