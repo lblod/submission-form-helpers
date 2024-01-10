e58ffb994ee5ca6cd11a93f8934eef433efec4d9
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2024-01-10 18:07:10 +0100

    Add some more `valid-date` constraint tests

--- tests/constraints/valid-date.test.js
+++ tests/constraints/valid-date.test.js
@@ -3,27 +3,31 @@ import { Literal } from "rdflib";
 import validDate from "../../src/constraints/valid-date.js";
 
 test('it validates if the provided literal is a valid "xsd:date"', (t) => {
-  let dateLiteral = new Literal(
-    "2022-10-12",
-    null,
-    "http://www.w3.org/2001/XMLSchema#date"
-  );
-  t.true(validDate(dateLiteral));
+  t.true(validDate(dateLiteral("2022-10-12")));
 
-  dateLiteral = new Literal(
-    "2022-14-32",
-    null,
-    "http://www.w3.org/2001/XMLSchema#date"
-  );
-  t.false(validDate(dateLiteral), "unrealistic dates return false");
-
-  dateLiteral = new Literal(
-    "2022-10-12T12:01:52Z",
-    null,
-    "http://www.w3.org/2001/XMLSchema#dateTime"
-  );
   t.false(
-    validDate(dateLiteral),
+    validDate(dateLiteral("2022-14-32")),
+    "unrealistic dates return false"
+  );
+
+  t.false(
+    validDate(dateLiteral("2022-10-12T12:01:52Z")),
     "xsd:dateTime literals aren't considered valid"
   );
+
+  t.false(validDate(dateLiteral("")), "empty strings aren't considered valid");
+
+  t.false(validDate(Literal.fromNumber(42)), "xsd:integer values return false");
+  t.false(
+    validDate(Literal.fromNumber(12.34)),
+    "xsd:decimal values return false"
+  );
+  t.false(
+    validDate(Literal.fromBoolean(true)),
+    "xsd:boolean values return false"
+  );
 });
+
+function dateLiteral(dateString) {
+  return new Literal(dateString, null, "http://www.w3.org/2001/XMLSchema#date");
+}
