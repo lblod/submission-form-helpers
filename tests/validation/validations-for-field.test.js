f024ab00b29008e315eb09e2b46c99859572f7a8
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2024-01-18 15:18:20 +0100

    Add "severity" support to the `validationsForField` helper
    By default it will return all validations that don't have a `sh:sevirity` predicate or the `sh:Violation` value.
    
    It's possible to get _only_ the warning or info violations by passing in the corresponding predicate.

--- tests/validation/validations-for-field.test.js
+++ tests/validation/validations-for-field.test.js
@@ -2,7 +2,7 @@ import test from "ava";
 import { NamedNode } from "rdflib";
 import ForkingStore from "forking-store";
 import { readFixtureFile } from "../test-helpers.js";
-import { RDF, FORM, validationsForField } from "../../src/index.js";
+import { RDF, FORM, validationsForField, SHACL } from "../../src/index.js";
 import { EXT } from "../namespaces.js";
 
 const FORM_GRAPHS = {
@@ -12,7 +12,7 @@ const FORM_GRAPHS = {
 };
 
 test("it returns all the validations for a specific field, as defined in the form graph", (t) => {
-  const formTtl = readFixtureFile("validation/validations-for-field/form.ttl");
+  const formTtl = readFixtureFile("validation/severity/form.ttl");
 
   let store = new ForkingStore();
   store.parse(formTtl, FORM_GRAPHS.formGraph, "text/turtle");
@@ -24,7 +24,7 @@ test("it returns all the validations for a specific field, as defined in the for
     FORM_GRAPHS.formGraph
   );
 
-  let fieldUri = EXT('inputField');
+  let fieldUri = EXT("inputField");
 
   let validations = validationsForField(fieldUri, {
     store,
@@ -32,5 +32,49 @@ test("it returns all the validations for a specific field, as defined in the for
     ...FORM_GRAPHS,
   });
 
-  t.true(validations.length === 1);
+  t.true(
+    validations.length === 2,
+    "sh:Warning violations are ignored for backwards compatibility reasons"
+  );
+});
+
+test("it accepts a severity value to filter the returned validations", (t) => {
+  const formTtl = readFixtureFile("validation/severity/form.ttl");
+
+  let store = new ForkingStore();
+  store.parse(formTtl, FORM_GRAPHS.formGraph, "text/turtle");
+
+  const form = store.any(
+    undefined,
+    RDF("type"),
+    FORM("Form"),
+    FORM_GRAPHS.formGraph
+  );
+
+  let fieldUri = EXT("inputField");
+
+  let validations = validationsForField(fieldUri, {
+    store,
+    form,
+    ...FORM_GRAPHS,
+  });
+
+  t.true(
+    validations.length === 2,
+    "It ignores sh:Warning validations by default"
+  );
+
+  let warningValidations = validationsForField(fieldUri, {
+    store,
+    form,
+    ...FORM_GRAPHS,
+    severity: SHACL("Warning"),
+  });
+
+  t.true(warningValidations.length === 1);
+  t.notDeepEqual(
+    warningValidations,
+    validations,
+    "It only returns the warnings when we provide a `sh:Warning` node"
+  );
 });
